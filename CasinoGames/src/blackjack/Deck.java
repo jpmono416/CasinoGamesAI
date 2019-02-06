b305dfcc7b016c8979c8828232c08b652ef7e95e
n0753746 <n0753746@my.ntu.ac.uk>
2019-02-06 14:49:04 +0000

    Deck Class almost finished

--- CasinoGames/src/blackjack/Deck.java
+++ CasinoGames/src/blackjack/Deck.java
@@ -9,25 +9,58 @@ import blackjack.Card;;
 
 public class Deck {
 
-	private final Integer cardsOnADeck = 52;
-	//private enum CardSuits { Hearts, Clubs, Diamonds, Spades };
-	private List<Card> remainingCards;
-	private List<Card> discardedCards;
 	
+	//private enum CardSuits { Hearts, Clubs, Diamonds, Spades };
+	
+	private final Integer cardsOnADeck = 52;
+	public Integer getCardsOnADeck() {
+		return cardsOnADeck;
+	}
+	
+	private final Integer cardsNotShuffled = 20;
+	public Integer getCardsNotShuffled() {
+		return cardsNotShuffled;
+	}
+
+	private List<Card> remainingCards;
+	public List<Card> getRemainingCards() { 
+		return remainingCards;
+	}
+	public void setRemainingCards(List<Card> remainingCards) {
+		this.remainingCards = remainingCards;
+	}
+	
+	private List<Card> discardedCards;
+	public List<Card> getDiscardedCards() {
+		return discardedCards;
+	}
+	public void setDiscardedCards(List<Card> discardedCards) {
+		this.discardedCards = discardedCards;
+	}
+	
+	
+
 	public Deck(Integer amountOfDecks)
 	{
-		final Integer totalAmountOfCards = amountOfDecks * this.cardsOnADeck;
+		final Integer totalAmountOfCards = amountOfDecks * getCardsOnADeck();
 		remainingCards = new ArrayList<>(totalAmountOfCards);
 		discardedCards = new ArrayList<>(totalAmountOfCards);
 		Integer counter = 1;
 		
 		while(counter <= amountOfDecks)
 		{
-			this.remainingCards.addAll(buildDeck());
+			getRemainingCards().addAll(buildDeck());
 		}
 		 // TODO POSSIBLE IMPROVEMENT -> SAVE LIST AND JUST REPEAT
 	}
 	
+	
+	public Card drawCard()
+	{
+		
+		
+		return null;
+	}
 	
 	public List<Card> buildDeck()
 	{
@@ -49,15 +82,27 @@ public class Deck {
 		return cardsOfDeck;
 	}
 	
-	public void shuffleDeck(Integer extraCardsInDeck, Deck cardsToShuffle)
+	// Method for shuffling only a few discarded cards into the deck
+	public void shuffleDeck(Boolean shuffleWholeDeck)
 	{
+		// Only shuffling discarded, add cards that are
+		// yet to be processed and shuffled by the machine
+		Integer countOfCards = getDiscardedCards().size() + getCardsNotShuffled();
+		
+		if(shuffleWholeDeck)
+		{
+			countOfCards = getRemainingCards().size() + getDiscardedCards().size();
+		}
+		// Ensure the whole deck is in place 
+		getRemainingCards().addAll(getDiscardedCards());
+		
+		
+		// Implementation of a Fisher-Yates shuffling algorithm
 		
 	}
 	
-	public void shuffleDeck()
-	{
-		this.shuffleDeck(0, this);
-	}
+	// Overlap method for shuffling the whole deck rather than some cards
+	public void shuffleDeck() { shuffleDeck(true); }
 	
 	/**
 	 * Sort the whole deck and put it back into order.
@@ -66,13 +111,18 @@ public class Deck {
 	 */
 	public void sortDeck()
 	{
-		if(this.discardedCards.size() >= 1)
+		if(getDiscardedCards().size() >= 1)
 		{
-			this.remainingCards.addAll(this.discardedCards);
+			getRemainingCards().addAll(getDiscardedCards());
 		}
 		
-		Integer amountOfDecks = this.remainingCards.size() / cardsOnADeck;
+		Integer amountOfDecks = getRemainingCards().size() / cardsOnADeck;
 		
 		// TODO FINISH -> COMPARABLE
+	}
+	
+	private void swap()
+	{
+		
 	}
 }
